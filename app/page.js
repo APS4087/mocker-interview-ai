0d9f5fc6d7ace1aafd57129c684fd79cc6d775ea
APS4087 <aungpyaesoe.bgs@Gmail.com>
2024-06-15 12:32:40 +0800

    implementing clerk auth

--- app/page.js
+++ app/page.js
@@ -1,12 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
       <h1>mock-interview-ai</h1>
       <p>Using AI to practice your interview skills</p>
-      <Button>Get Started</Button>
     </div>
   );
 }
